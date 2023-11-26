7a59661a93f6eb6b81c1f38b09ce8bfcf47c8a87
ivikk <7197989+vikky783@users.noreply.github.com>
2023-11-26 08:12:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+MAX_LINES = 3;
+def deposit():
+    while True:
+        amount = input("What would you   like to deposit? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number. ")
+    return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input("What line would you like to bet on ? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines < MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number. ")
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+
+main()
